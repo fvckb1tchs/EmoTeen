3d701fac5319d6dd69215cae38dea04459d2038e
fvckb1tchs <emoteen-contato@outlook.com.br>
2025-08-20 11:03:09 -0300

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 //TROCAR URL E API
-const SUPABASE_URL = "https://.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = ".eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpiempub3lmYm53eW55dnR5bmdiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQzNTA3MjcsImV4cCI6MjA2OTkyNjcyN30.uh9mcFaqyJIqZEHgrs1USQy2OSF-dgNzp97Dalws4T4";
+const SUPABASE_URL = "https://fpozoioeptqymexmezmy.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = ".eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZwb3pvaW9lcHRxeW1leG1lem15Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUzNjY2MTYsImV4cCI6MjA3MDk0MjYxNn0.gPZSjKaURU0R7S7IXxuLg6DAQUwT0O-CbNqZ9M1GPqo";
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
